051e1272cea9301a3f4aa14a92905c37f9137163
Arman Khan <sparmankhan04@gmail.com>
2025-07-25 13:31:35 +0000

    use Noto Sans Bengali google font all bangla text

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Inter', 'sans-serif'],
+        body: ['"Noto Sans Bengali"', 'sans-serif'],
+        headline: ['"Noto Sans Bengali"', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
