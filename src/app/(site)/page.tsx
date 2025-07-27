6b69eac8c4b53ac71db9ae852b1600ac465b4f6e
Arman Khan <sparmankhan04@gmail.com>
2025-07-27 08:31:36 +0000

    show image gallery before video gallery in home page

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -3,6 +3,7 @@ import AboutSchool from '@/components/homepage/about-school';
 import TeachersCarousel from '@/components/homepage/teachers-carousel';
 import VideoGallery from '@/components/homepage/video-gallery';
 import ImportantLinks from '@/components/homepage/important-links';
+import ImageGallery from '@/components/school-details/image-gallery';
 
 
 export default function Home() {
@@ -19,6 +20,11 @@ export default function Home() {
         <div className="container mx-auto px-4">
             <TeachersCarousel />
         </div>
+        </div>
+        <div className="bg-white py-12 sm:py-16 lg:py-20">
+            <div className="container mx-auto px-4">
+                <ImageGallery />
+            </div>
         </div>
         <VideoGallery />
     </div>
